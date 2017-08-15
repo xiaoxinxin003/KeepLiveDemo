6a8a32c02a29f4aeb0c0fb9879ac0f9aa3ad8303
yang <yangxinxiao@123juzi.com>
2017-08-15 16:48:18 +0800

    native 方法中增加log输出

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-
+LOCAL_LDLIBS :=-llog
 LOCAL_MODULE := MyLibrary
 LOCAL_SRC_FILES =: MyLibrary.cpp
 include $(BUILD_SHARED_LIBRARY)
--- app/src/main/jni/MyLibrary.cpp
+++ app/src/main/jni/MyLibrary.cpp
@@ -2,15 +2,24 @@
 // Created by focus on 2017/8/14.
 //
 #include "com_yang_focus_keeplivedemo_MyNdk.h"
+#include <android/log.h>
+#include <jni.h>
+
+#define  LOG_TAG    "native-dev"
+#define  LOGI(...)  __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
+#define LOGE(...)  __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
+#define LOGI(...)  __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
 
 JNIEXPORT jstring JNICALL Java_com_yang_focus_keeplivedemo_MyNdk_getString
     (JNIEnv * env, jobject obj) {
+    LOGI("Java_com_yang_focus_keeplivedemo_MyNdk_getString");
      return (*env).NewStringUTF("this is mylibrary");
     }
 
 
 JNIEXPORT void JNICALL Java_com_yang_focus_keeplivedemo_MyNdk_displaySomething
   (JNIEnv * env, jobject obj) {
+    LOGI("Java_com_yang_focus_keeplivedemo_MyNdk_displaySomething");
     jclass ClassCJM = (*env).FindClass("com/yang/focus/keeplivedemo/MyNdk");
     jmethodID MethodDisplayMessage = (*env).GetMethodID(ClassCJM, "displayMessage", "(Ljava/lang/String;)V");
     jstring value = (*env).NewStringUTF("Hello World!");
